d7a024ec0b7352613f648f2d5a37fab91edbed90
Usama Azzam <ggmavenir@gmail.com>
2018-10-10 22:45:45 -0400

    lab13

---
+++ if-test/if-hostname.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+hostname = 'MTG'
+hostname = input("Please Enter a hostname: ")
+
+
+if hostname.lower() == 'mtg':
+  print("The hostname was found to be", hostname)
+  print("The hostname matches expected config", hostname)
+
+print("Existing Script")
+
